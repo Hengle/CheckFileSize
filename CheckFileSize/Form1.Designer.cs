083372a46c73af724156ddde41468c216d0e8229
coding2233 <coding2233@163.com>
2016-10-09 11:13:52 +0800

    初始工程，有检测文件的基本功能

---
+++ CheckFileSize/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CheckFileSize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.OpenFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenFolder
+            // 
+            this.OpenFolder.Location = new System.Drawing.Point(13, 13);
+            this.OpenFolder.Name = "OpenFolder";
+            this.OpenFolder.Size = new System.Drawing.Size(75, 23);
+            this.OpenFolder.TabIndex = 0;
+            this.OpenFolder.Text = "OpenFolder";
+            this.OpenFolder.UseVisualStyleBackColor = true;
+            this.OpenFolder.Click += new System.EventHandler(this.OnOpenFolderClick);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "文件夹路径:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(171, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(226, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(407, 271);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(432, 325);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OpenFolder);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "检查所有文件大小";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ CheckFileSize/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckFileSize
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitDataViewColumn();
+
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    int index = dataGridView1.Rows.Add();
+            //    dataGridView1.Rows[index].ReadOnly = true;
+            //    dataGridView1.Rows[index].Cells["name"].Value = "名称";
+            //    dataGridView1.Rows[index].Cells["path"].Value = "路径path**********************";
+            //    dataGridView1.Rows[index].Cells["size"].Value = "135";
+                
+            //}
+        }
+
+        //大于100M
+        float _FSizeValue = 100.0f;
+
+        #region 初始化dataview的列
+        private void InitDataViewColumn()
+        {
+            //去掉左边那一列
+            dataGridView1.RowHeadersVisible = false;
+            //去掉下面哪一行
+            dataGridView1.AllowUserToAddRows = false;
+
+            int columnsIndex = dataGridView1.Columns.Add("ID", "ID");
+            dataGridView1.Columns[columnsIndex].Width = 50;
+            //不可点击
+            dataGridView1.Columns[columnsIndex].SortMode = DataGridViewColumnSortMode.NotSortable;
+            columnsIndex = dataGridView1.Columns.Add("name", "文件名称");
+            dataGridView1.Columns[columnsIndex].Width = 100;
+            dataGridView1.Columns[columnsIndex].SortMode= DataGridViewColumnSortMode.NotSortable;
+            columnsIndex = dataGridView1.Columns.Add("path", "文件路径");
+            dataGridView1.Columns[columnsIndex].Width = 180;
+            dataGridView1.Columns[columnsIndex].SortMode = DataGridViewColumnSortMode.NotSortable;
+            columnsIndex = dataGridView1.Columns.Add("size", "文件大小");
+            dataGridView1.Columns[columnsIndex].Width = 100;
+            dataGridView1.Columns[columnsIndex].SortMode = DataGridViewColumnSortMode.NotSortable;
+        }
+        #endregion
+
+        #region 打开文件夹路径
+        private void OnOpenFolderClick(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result= fbd.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = fbd.SelectedPath;
+                //Console.WriteLine("点击确定");
+                GetAllFilesFromFolderPath(fbd.SelectedPath);
+            }
+        }
+        #endregion
+
+        #region 通过文件夹路径获取所有文件
+        private void GetAllFilesFromFolderPath(string path)
+        {
+            DirectoryInfo RootFolder = new DirectoryInfo(path);
+
+            FileSystemInfo[] Fsi = RootFolder.GetFileSystemInfos();
+            for (int i = 0; i < Fsi.Length; i++)
+            {
+                if (Fsi[i] is DirectoryInfo)//是文件夹
+                {
+                    string _TempPath = (Fsi[i] as DirectoryInfo).FullName;
+                    GetAllFilesFromFolderPath(_TempPath);
+                }
+                if (Fsi[i] is FileInfo)//是文件
+                {
+                    FileInfo _TempFileInfo = Fsi[i] as FileInfo;
+                    Console.WriteLine(_TempFileInfo.Name);
+                    float _TempFileSize = (float)_TempFileInfo.Length / (1024.0f * 1024.0f);
+                    if (_TempFileSize>_FSizeValue)
+                        AddFileInforItemToDataViewRows(_TempFileInfo);
+                }
+            }
+            
+        }
+        #endregion
+
+        #region 通过文件信息添加数据列表
+        void AddFileInforItemToDataViewRows(FileInfo _TempFile)
+        {
+            string _TempFileName = _TempFile.Name;
+            string _TempFilePath = _TempFile.FullName;
+            //1024字节=1kb  1024kb=1MB
+            float _TempFileSize=(float)_TempFile.Length/(1024.0f*1024.0f);
+
+            int index = dataGridView1.Rows.Add();
+            dataGridView1.Rows[index].ReadOnly = true;
+            dataGridView1.Rows[index].Cells["ID"].Value = index;
+            dataGridView1.Rows[index].Cells["name"].Value = _TempFileName;
+            dataGridView1.Rows[index].Cells["path"].Value = _TempFilePath;
+            dataGridView1.Rows[index].Cells["size"].Value = _TempFileSize + "MB";
+        }
+        #endregion
+
+    }
+}
